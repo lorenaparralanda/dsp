6705575ae1fd8c38994bba63bcac6f65811522bf
lorenaparralanda <lorenadlpl@gmail.com>
2016-12-23 10:28:29 -0500

    Added code for date exercises

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,33 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+from datetime import datetime
+
+def days_between_dashes(d1, d2):
+    d1 = datetime.strptime(d1, "%m-%d-%Y")
+    d2 = datetime.strptime(d2, "%m-%d-%Y")
+    return abs((d2-d1).days)
+
+days_between_dashes (date_start, date_stop)
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+def days_between_joint(d1, d2):
+    d1 = datetime.strptime(d1, "%m%d%Y")
+    d2 = datetime.strptime(d2, "%m%d%Y")
+    return abs((d2-d1).days)
+
+days_between_joint (date_start, date_stop)
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+from dateutil import parser
+
+d1 = (parser.parse(date_start)).strftime("%m-%d-%Y")
+d2 = (parser.parse(date_stop)).strftime("%m-%d-%Y")
+
+days_between_dashes(d1, d2)
