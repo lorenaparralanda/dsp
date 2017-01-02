ad4545bfbcb9a25b61748015a0330edeb2429e29
lorenaparralanda <lorenadlpl@gmail.com>
2017-01-01 23:03:08 -0500

    Added code to csv exercise

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
 PLACE YOUR CODE HERE
+
+# CONTINUED FROM ADVANCED PYTHON REGEX EXERCISES 
+
+for email in df_email_list:
+    print email
+    
+import csv
+
+with open("Email list.csv", "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for val in df_email_list:
+        writer.writerow([val])
